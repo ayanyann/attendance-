55cb436cb5800bfaddc1955fbe2a8ed8b6fe3e7b
ayanyann <125654465+ayanyann@users.noreply.github.com>
2024-04-07 10:27:53 +0800

    Update quick-attendance-app.py

--- quick-attendance-app.py
+++ quick-attendance-app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
+import plotly.express as px
+
 
 # Display Title and Description
 st.title("CCS Quick Attendance Portal")
