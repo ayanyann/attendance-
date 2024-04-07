88a9ba836e284075b3daadf500395c495e241305
ayanyann <125654465+ayanyann@users.noreply.github.com>
2024-04-07 10:48:49 +0800

    Update quick-attendance-app.py

--- quick-attendance-app.py
+++ quick-attendance-app.py
@@ -3,7 +3,7 @@ from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import plotly.express as px
 
-
+col1, col2 = st.columns(2)
 # Display Title and Description
 st.title("CCS Quick Attendance Portal")
 st.markdown("enter your sins below")
@@ -70,19 +70,21 @@ if action == "Magpa Attendance":
 
 elif action == "Mo lantaw sa naka Attendance":
     st.dataframe(existing_data)
-     # Count the occurrences of each "Year" in the "Year" column
-    year_counts = existing_data["Year"].value_counts().reset_index()
-    year_counts.columns = ['Year', 'Count']  # Renaming columns for clarity
-    
-    # Create a bar chart using Plotly
-    fig = px.bar(year_counts, x='Year', y='Count', title='Attendance by Year')
-    st.plotly_chart(fig)  # Display the figure in the Streamlit app
+    with col1:
+        # Aggregate data by 'Year' to get the count of attendance per year
+        year_counts = existing_data['Year'].value_counts().reset_index()
+        year_counts.columns = ['Year', 'Count']  # Renaming columns for clarity
 
+        # Create a bar chart using Plotly
+        fig = px.bar(year_counts, x='Year', y='Count', title='Attendance Count by Year')
+
+        # Display the figure in the Streamlit app
+        st.plotly_chart(fig)
+    with col2:
+        # For Pie Chart of Majors
+        major_counts = existing_data["Major"].value_counts().reset_index()
+        major_counts.columns = ['Major', 'Count']  # Renaming columns for clarity
     
-     # For Pie Chart of Majors
-    major_counts = existing_data["Major"].value_counts().reset_index()
-    major_counts.columns = ['Major', 'Count']  # Renaming columns for clarity
-    
-    # Create a pie chart using Plotly
-    pie_fig = px.pie(major_counts, values='Count', names='Major', title='Distribution of Majors')
-    st.plotly_chart(pie_fig)  # Display the pie chart in the Streamlit app
+        # Create a pie chart using Plotly
+        pie_fig = px.pie(major_counts, values='Count', names='Major', title='Distribution of Majors')
+        st.plotly_chart(pie_fig)  # Display the pie chart in the Streamlit app)
